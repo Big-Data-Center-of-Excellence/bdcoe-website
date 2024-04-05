34fd13a4a6a29d1acb90217f6435d5c407704bb0
traptijindal <traptijindal5678@gmail.com>
2024-04-05 17:45:26 +0530

    changes

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -9,6 +9,7 @@ import technolite from '../assets/images/technolite.jpeg';
 import africa from '../assets/images/africa.jpeg';
 import zonals from '../assets/images/zonals.jpg';
 import anveshna from '../assets/images/anveshna.jpg';
+import Kavach from '../assets/images/Kavach.jpeg';
 import Jodhpur from '../assets/images/Jodhpur.jpg'
 import Footer from '../components/footer/footer';
 import './Achievements.css';
@@ -33,14 +34,14 @@ function Achievements() {
          <img src="https://www.akgec.ac.in/wp-content/uploads/2023/03/ITAchieve2.jpeg" alt=""className='img1' />
          <div className='cnt'>
          <p className='labels'> Winner: SIH 2022</p>
-          <p className='text'>Our 4th year coordinator Sankalp Srivastava and his team IDEAL-BITS have clinched first position in SIH.</p>
+          <p className='text'>Our 4th year coordinator, Sankalp Srivastava and his team IDEAL-BITS have clinched first position in SIH.</p>
          </div>
          </div>
          <div className='sub-parent'>
           <img src={zonals} alt="" className=' img1'/>
          <div className='cnt'>
          <p className='labels'>Winner: AKTU Zonals 2022</p>
-         <p className='text'>Our 4th-year coordinator, Abhishek Gupta, along with his teammate Bhavya Agrawal, has secured  2nd rank in AKTU Zonals event Game of Codes.</p>
+         <p className='text'>Our 4th-year coordinator, Abhishek Gupta, along with his teammate Bhavya Agrawal has secured 2nd rank in AKTU Zonals event Game of Codes.</p>
          </div>
          </div>
          <div className='sub-parent '>
@@ -54,21 +55,21 @@ function Achievements() {
          <img src={anveshna} alt=""className='img1' />
          <div className='cnt'>
           <p className='labels'>Winner: Anveshna 2024</p>
-         <p className='text'>Our 3rd-year coordinators, Harsh Sharma and Tarun Lalwani, alongside Manya Singhal, a 2nd-year member, and their team 'Saksham', have performed very well in Anveshna organized by Agastya International Foundation.</p>
+         <p className='text'>Our 3rd-year coordinators, Harsh Sharma and Tarun Lalwani alongside Manya Singhal, a 2nd-year member and their team 'Saksham' have performed very well in Anveshna organized by Agastya International Foundation.</p>
          </div>
          </div>
          <div className='sub-parent'>
           <img src={Jodhpur} alt="" className='img1'/>
          <div className='cnt'>
           <p className='labels'>Winner: Prometeo 2024</p>
-         <p className='text' > Our 2nd-year member, Krish Kumar, and his team, Team !404: Coding Triumph, emerged victorious in Prometeo Hackathon held at IIT Jodhpur.</p>
+         <p className='text' > Our 2nd-year member, Krish Kumar and his team Team !404: Coding Triumph emerged victorious in Prometeo Hackathon held at IIT Jodhpur.</p>
          </div>
          </div>
          <div className='sub-parent '>
          <img src={rise} alt=""  className='img1' />
          <div className='cnt'>
          <p className='labels'>Winner:  RIISE 2024</p>
-         <p className='text'>Our 3rd-year coordinators, Harsh Sharma, Tarun Lalwani, and Ketan Singh, along with their team Saksham, have secured first position in RIISE, organized by Indraprastha IIIT Delhi.</p>
+         <p className='text'>Our 3rd-year coordinators, Harsh Sharma, Tarun Lalwani and Ketan Singh along with their team Saksham have secured first position in RIISE organized by Indraprastha IIIT Delhi.</p>
          <p></p>
          </div>
          </div>
@@ -76,7 +77,7 @@ function Achievements() {
           <img src={africa} alt="" className=' img1'/>
          <div className='cnt'>
          <p className='labels'>Finalist: UNESCO Africa Hackathon 2022</p>
-         <p className='text'>Our 4th-year coordinator, Prateek Chaurasia, and his team AGRICODE have earned a spot in the finals of the UNESCO Africa Hackathon.</p>
+         <p className='text'>Our 4th-year coordinator, Prateek Chaurasia and his team AGRICODE have earned a spot in the finals of the UNESCO Africa Hackathon.</p>
          </div>
          </div>
         
@@ -85,7 +86,7 @@ function Achievements() {
           <img src={SIH2} alt="" className='img1'/>
          <div className='cnt'>
           <p className='labels'>Finalist : SIH 2023</p>
-         <p className='text'>Our 4th-year co-ordinators, Mayank Johari and Somya Maheshwari, along with their team Nyaysetu, have earned a spot in finals of the Smart India Hackathon.</p>
+         <p className='text'>Our 4th-year co-ordinators, Mayank Johari and Somya Maheshwari along with their team Nyaysetu have earned a spot in finals of the Smart India Hackathon.</p>
          </div>
           </div>
          
@@ -93,21 +94,28 @@ function Achievements() {
           <img src={finalist} alt="" className=' img1 img2'/>
          <div className='cnt'>
           <p className='labels'>Finalist : SIH 2023</p>
-         <p className='text'> Our 4th-year coordinator, Sankalp Srivastava, and his team Anthracite 2.0, have earned a spot in finals of the Smart India Hackathon 2023.</p>
+         <p className='text'> Our 4th-year coordinator, Sankalp Srivastava and his team Anthracite 2.0 have earned a spot in finals of the Smart India Hackathon 2023.</p>
          </div>
          </div>
          <div className='sub-parent'>
           <img src={technolite} alt="" className=' img1 '/>
          <div className='cnt'>
          <p className='labels'>Finalist : Technolite 2023</p>
-         <p className='text'>Our 4th-year coordinator, Sankalp Srivastava, and his team Genesis, have earned a spot in the finals of Technolite, organized by Delhi Skills and Entrepreneurship University.</p>
+         <p className='text'>Our 4th-year coordinator, Sankalp Srivastava and his team Genesis have earned a spot in the finals of Technolite, organized by Delhi Skills and Entrepreneurship University.</p>
          </div>
          </div>
          <div className='sub-parent '>
          <img src={innohacks} alt=""  className='img1' />
         <div className='cnt'>
         <p className='labels'>Finalist: Innohacks 2023</p>
-        <p className='text'>Our 3rd-year coordinator, Atishay Jain, and his team TECH-BELLATORS, have performed exceptionally well in Innohacks organized by KIET.</p>
+        <p className='text'>Our 3rd-year coordinator, Atishay Jain and his team TECH-BELLATORS was selected as finalist in Innohacks organized by KIET.</p>
+        </div>
+         </div>
+         <div className='sub-parent '>
+         <img src={Kavach} alt=""  className='img1' />
+        <div className='cnt'>
+        <p className='labels'>Finalist: Kavach 2023</p>
+        <p className='text'>Our 4th year coordinators, Prateek Chaurasia, Abhishek Gupta, Anshika Agarwal and their team PROXYMORONS were selected as finalist in Kavach.</p>
         </div>
          </div>
         
