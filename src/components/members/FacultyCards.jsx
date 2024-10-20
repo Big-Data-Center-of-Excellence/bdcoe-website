78ca2e13a3bcac2639c776665965c888623de6e5
Mohitxroxx <mohitsinghroxx@gmail.com>
2024-10-20 20:10:40 +0530

    Updated faculty coordinator

--- src/components/members/FacultyCards.jsx
+++ src/components/members/FacultyCards.jsx
@@ -3,10 +3,10 @@ import './card.css'
 import './album.css'
 import cardBackground from '../../assets/images/cardBack.svg'
 
-
 function FacultyCards() {
   return (
     <div className='container-album'>
+
       <div className='child-album' style={{ paddingBottom: "1em" }}>
         <img
           className='background-card'
@@ -21,6 +21,8 @@ function FacultyCards() {
         <h4>Associate Professor</h4>
 
       </div>
+
+
       <div className='child-album' style={{ paddingBottom: "1em" }}>
         <img
           className='background-card'
@@ -28,12 +30,28 @@ function FacultyCards() {
           alt="back"
         />
         <img className='overlay'
-          src="https://www.akgec.ac.in/wp-content/uploads/2023/04/achintya.jpg"
+          src="http://res.cloudinary.com/dhy42cidp/image/upload/v1729435141/ytazhk1sgbv8do71fga6.jpg"
           alt='bdcoe'
         />
-        <h3 style={{ paddingBottom: "1em" }}>Mr. Achintya Pandey</h3>
+        <h3 style={{ paddingBottom: "1em" }}>Mr. Amit Kumar</h3>
         <h4>Assistant Professor</h4>
       </div>
+
+
+      {/* <div className='child-album' style={{ paddingBottom: "1em" }}>
+        <img
+          className='background-card'
+          src={cardBackground}
+          alt="back"
+        />
+        <img className='overlay'
+          src=""
+          alt='bdcoe'
+        />
+        <h3 style={{ paddingBottom: "1em" }}>Mr. Ropak</h3>
+        <h4>Assistant Professor</h4>
+      </div> */}
+
     </div>
   )
 }
